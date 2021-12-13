066cfcb3094c2957f7bee21aefd4680bd1d5d28c
Autor <civimip573@gyn5.com>
2021-12-13 13:19:19 -0400

    init

---
+++ src/main/java/org/jbtc/gshop/GshopApplication.java
@@ -0,0 +1,13 @@
+package org.jbtc.gshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jbtc/gshop/GshopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jbtc.gshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
